94ab03a37c3c20ac007cc26ad947678765d98a92
Matilde Morrone <contact@morrone.dev>
2025-06-10 23:17:40 +0200

    Parse some more information

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,28 @@ use std::{
 use anyhow::Result;
 use bytemuck::cast_slice;
 
+fn read_str<'a>(buf: &'a mut [u8], rom: &mut File) -> Result<&'a str> {
+    rom.read_exact(buf)?;
+    Ok(str::from_utf8(buf)?.trim_end_matches('\0'))
+}
+
 fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
 
     let mut rom = File::open(&args[1])?;
 
-    let mut name = [0u8; 12];
-    rom.read_exact(&mut name)?;
-    let name = str::from_utf8(&name)?.trim_end_matches('\0');
+    let mut game_title = [0u8; 12];
+    let game_title = read_str(&mut game_title, &mut rom)?;
 
-    println!("Name: {name}");
+    let mut gamecode = [0u8; 4];
+    let gamecode = read_str(&mut gamecode, &mut rom)?;
+
+    let mut makercode = [0u8; 2];
+    let makercode = read_str(&mut makercode, &mut rom)?;
+
+    println!("Game Title: {game_title}");
+    println!("Gamecode: {gamecode}");
+    println!("Makercode: {makercode}");
 
     rom.seek(SeekFrom::Start(0x68))?;
 
@@ -29,7 +41,15 @@ fn main() -> Result<()> {
     rom.read_exact(&mut title)?;
     let title = String::from_utf16(cast_slice(&title))?;
 
-    println!("Title: {}", title.trim_end_matches('\0'));
+    println!("-------Title-------");
+    println!("{}", title.trim_end_matches('\0'));
+    println!("-------------------");
 
     Ok(())
 }
+
+// pub struct Title {}
+
+// fn parse_title() -> Result<()> {
+//     Ok(())
+// }
