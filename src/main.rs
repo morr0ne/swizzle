321d5590b07841ee1c50c9ed98f7f08ce230d826
Matilde Morrone <contact@morrone.dev>
2025-06-10 22:37:01 +0200

    Publish some basic parsing code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::{self, File};
+
+use anyhow::{Result, bail};
+use bytemuck::cast_slice;
+
+fn main() -> Result<()> {
+
+
+    let name = &rom[0..12];
+    let name = str::from_utf8(&[
+        b'P', b'A', b'L', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ])?
+    .trim_end_matches('\0');
+
+    println!("Name: {}", name);
+
+    dbg!(name.as_bytes());
+
+    let title_offset = &rom[0x68..0x68 + 4];
+
+    let title_offset = u32::from_le_bytes([
+        title_offset[0],
+        title_offset[1],
+        title_offset[2],
+        title_offset[3],
+    ]) as usize;
+
+    if title_offset == 0 {
+        bail!("WAAAAAA")
+    }
+
+    let title_header = &rom[title_offset..];
+
+    let title = &title_header[0x240..0x240 + 100];
+
+    println!("Title: {}", String::from_utf16_lossy(cast_slice(title)));
+
+    Ok(())
 }
